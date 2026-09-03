5587d5c3949159b4530681f3879500e6965c83a1
essarhirabdellah <essrhirabdellah3@gmail.com>
2026-09-03 17:42:12 +0200

    this me i did not finsh the code yet

---
+++ chalenge-code.js
@@ -0,0 +1,33 @@
+function even(start,over){
+    while(start <= over){
+        if(start%2 == 0){
+            console.log(`this number ${start} even`);
+        }
+        start++
+    }
+}
+even(1,100);
+
+function check_even(start,over){
+    let i = start;
+    let sum = 0;
+    while(i <= over){
+       loopme = start + over + i;
+       if(loopme%2 == 0) {
+        sum += loopme;
+       }
+       i++;
+    }
+    return `this the sum of all even numbers ${sum}`;
+}
+console.log(check_even(1,10));
+
+function tringle(size){
+    for (let i = 0 ; i <= size ; i++) {
+        let line 
+        for(let x =  )
+        
+        
+    }
+}
+ 
--- chalenge-day-2.js
+++ chalenge-day-2.js
@@ -58,4 +58,49 @@ function factorial(n){
 
 }
 console.log(factorial(20))
+//arguments 
+function into(){
+    console.log(arguments[0])
+}
+into(3);
 
+//calculate
+function calculate(a, b, operation){
+    switch(operation){
+        case "*":
+            return a*b;
+            break;
+        case "+":
+            return a+b;
+            break;
+
+        case "-":
+            return a-b;
+            break;
+        default:
+
+           return `this operation ${operation} did not exact`;
+
+    }
+}
+console.log(calculate(1,8,"+"))
+
+
+//createMultiplier
+function createMultiplier(multiplier){
+    return  mul = (multiplier) =>  multiplier / 4 ;
+
+}
+console.log(createMultiplier(5))
+
+//check arry of elment
+let arr = [1,2,3,4]
+function filterOddNumbers(arr){
+  for (let i = 0; i <= arr.length; i++) {
+    if (i%2 == 0) {
+        console.log(`true, this number ${i} is odd`)
+    }
+    
+  }
+}
+filterOddNumbers(arr);
