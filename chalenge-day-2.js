f77ea9250e5f8772ae81b4cce553aa2c53a0d221
essarhirabdellah <essrhirabdellah3@gmail.com>
2026-09-03 15:14:36 +0200

    this is the scond level

--- chalenge-day-2.js
+++ chalenge-day-2.js
@@ -18,3 +18,44 @@ console.log(sum(5));
 //square func
 const square = function (x , y = 2) {return x ** y};
 console.log(4)
+
+//level two
+
+//tampreture 
+function celsiusToFahrenheit(c){
+    let n = (c * 9/5) + 32 ;
+    if (c > 26 && c < 30) {
+        return ` the weather is good ${n} `;
+    }
+    return n;
+}
+console.log(celsiusToFahrenheit(27))
+//make sure the string embty
+const isEmpty = (str) => {
+  for (const char in str){
+
+    if(str = ""){return "true"};
+    if (str = " ") {return "false"};
+
+}}
+let str = "lisa";
+console.log(isEmpty(str))
+
+//fact
+
+function factorial(n){
+    let risult = 0;
+    let max1 = n--
+    let max = n
+    while(max >= 1 && max >= 2 ) {
+        
+        risult = risult + max*max1
+        max--
+        max1--
+        
+    }
+    return `the fact is ${risult}`;
+
+}
+console.log(factorial(20))
+
